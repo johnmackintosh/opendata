6d84056a4008052ff65c4805d20e6549d16dbc71
Andy <andy.judson@gmail.com>
2014-01-25 13:04:37 +0000

    Restructured repos

--- worldbank/hacks.r
+++ worldbank/hacks.r
@@ -1,5 +1,5 @@
-setwd('/Users/andrewjudson/GitHub/local/opendata/data/worldbank')
-data<-read.csv('population.csv')
+# load data
+data<-read.csv('/Users/andrewjudson/GitHub/local/opendata/worldbank/population.csv')
 str(data)
 options(digits=10)
 summary(data$yr_2012)
